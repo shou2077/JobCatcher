b30eee6a5db58217dfca5d914c2ed902a636953d
Sen H <145161431+shou2077@users.noreply.github.com>
2025-02-25 23:16:25 +0000

    Pending changes exported from your codespace

---
+++ job-auto-apply-admin/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AdminDashboard from './components/AdminDashboard';
+
+function App() {
+  return (
+      <div className="App">
+        <AdminDashboard />
+      </div>
+    );
+}
+
+export default App;
